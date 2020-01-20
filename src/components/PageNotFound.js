45a329deb2833b918e442c0ccf10e03a631fd8d1
Yofetahe Habtu <yofetahe@gmail.com>
2020-01-19 22:39:15 -0800

    update on page not found page

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+import RedirectPage from './RedirectPage';
+
+ const linkStyle = {
+        border: "1px solid gray",
+        borderRadius: "5px",
+        padding: "5px 15px",
+        textDecoration: "none",
+        color: "#fff",
+        backgroundColor: "rgba(112, 112, 112, 0.5)"
+    }
+
+const h1Style = {
+    fontSize: "80px",
+    lineHeight: "1"
+}
 
 const PageNotFound = () => {
-    return <div>Page Not Found</div>
+   
+    return (
+        <Fragment>
+            <RedirectPage />
+            <div id="homePageContent" className="PageContent" style={{textAlign: "center", paddingTop: "50px"}}>
+                <p>OOPS! PAGE NOT FOUND</p>
+                <p style={h1Style}>404</p>
+                <p>I am sorry, the page you requested was not found</p>
+                <Link id="homeLink" to="/" style={linkStyle}>Go to HOME</Link>
+            </div>
+        </Fragment>
+    );
 }
 
 export default PageNotFound;
